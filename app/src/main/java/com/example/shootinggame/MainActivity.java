099e4ffdcf463524293124b37a8a8bd070b65808
kaito <t39kaitoemailaddress@gmail.com>
2023-07-29 21:53:49 +0900

    仮完成２

--- app/src/main/java/com/example/shootinggame/MainActivity.java
+++ app/src/main/java/com/example/shootinggame/MainActivity.java
@@ -3,7 +3,6 @@
  */
 
 package com.example.shootinggame;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.opengl.GLSurfaceView;
@@ -12,12 +11,10 @@ import android.view.WindowInsets;
 import android.view.WindowMetrics;
 import android.graphics.Insets;
 import android.view.WindowInsetsController;
-
 public class MainActivity extends Activity
 {
     private GLSurfaceView _glSurfaceView;
     private GameMaster _renderer;
-
     @Override
     //クラスを作成
     protected void onCreate(Bundle savedInstanceState)
@@ -27,28 +24,37 @@ public class MainActivity extends Activity
         _renderer = new GameMaster(this);
         _glSurfaceView.setRenderer(_renderer);
         setContentView(_glSurfaceView);
-
         //画面サイズを取得する
         WindowMetrics windowMetrics = getWindowManager().getCurrentWindowMetrics();
         Insets insets = windowMetrics.getWindowInsets().getInsetsIgnoringVisibility(WindowInsets.Type.systemBars());
-
         //ステータスバーのサイズを取得
         int statusBarHeight = getStatusBarHeight();
-
         //取得した画面サイズを代入
         _renderer._width = windowMetrics.getBounds().width() - statusBarHeight;
         _renderer._height = windowMetrics.getBounds().height();
-
         //ステータスバーとナビゲーションバーの両方を非表示にする
         WindowInsetsController windowInsetsController = getWindow().getInsetsController();
         if (windowInsetsController != null) {
             windowInsetsController.hide(WindowInsets.Type.systemBars());
             windowInsetsController.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
         }
-
-
     }
-
+    @Override
+    //フォーカスが再開したときに呼び出される関数
+    protected void onResume()
+    {
+        //再開する
+        super.onResume();
+        _glSurfaceView.onResume();
+    }
+    @Override
+    //フォーカスを失ったときに呼び出される関数
+    protected void onPause()
+    {
+        //一時停止する
+        super.onPause();
+        _glSurfaceView.onPause();
+    }
     //ステータスバーのサイズ（高さ）を取得する関数
     public int getStatusBarHeight() {
         int statusBarHeight = 0;
@@ -58,26 +64,6 @@ public class MainActivity extends Activity
         }
         return statusBarHeight;
     }
-
-    @Override
-    //フォーカスが再開したときに呼び出される関数
-    protected void onResume()
-    {
-        //再開する
-        super.onResume();
-        _glSurfaceView.onResume();
-
-    }
-
-    @Override
-    //フォーカスを失ったときに呼び出される関数
-    protected void onPause()
-    {
-        //一時停止する
-        super.onPause();
-        _glSurfaceView.onPause();
-    }
-
     @Override
     //画面がタッチされたときに呼び出される関数
     public boolean onTouchEvent(MotionEvent event)
